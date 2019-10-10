c4fce436f53c784c4a05aeba11991c8603b60cd7
JuanGA12 <juan.galvez@utec.edu.pe>
2019-10-10 18:40:04 -0500

    PC JUAN GALVEZ

--- heap.h
+++ heap.h
@@ -5,4 +5,86 @@
 #ifndef CS1103_PC2_201902_HEAP_H
 #define CS1103_PC2_201902_HEAP_H
 
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+template <typename Container>
+class heap{
+public:
+    Container vec;
+
+    auto size_(){
+        return vec.size();
+    }
+    auto get_max(){
+        int maximo = 0;
+        for(int i = 0; i < vec.size(); i++){
+            if(vec[i]>maximo){
+                maximo = vec[i];
+            }
+        }
+        std::cout<<"El maximo es: " <<maximo<<std::endl;
+    }
+    template <typename t>
+    auto insert( t x){
+        vec.push_back(x);
+    }
+    template <typename ...t>
+    auto insert(t ...items){
+        for (auto &v:{items...}){
+            vec.push_back(v);
+        }
+    }
+    void load_from(){
+
+        vector<int> c;
+        ifstream datos;
+        datos.open("data.txt",ios::in);
+
+        try{
+            if(datos.is_open()){
+                string valor;
+                while(datos.good()){
+                    getline(datos, valor);
+                    c.push_back(stoi(valor));
+                }
+            }else {
+                throw logic_error("No se pudo abrir el archivo .txt");
+            }
+        }
+        catch (std::exception& e){
+            cout<<e.what()<<endl;
+        }
+
+        for(int i = 0; i<c.size();i++){
+            vec.push_back(c[i]);
+        }
+    }
+
+
+    /*template <typename T>
+    friend std::istream& operator >> (std::istream& in, heap<Container>& heap){
+
+        T  x;
+        std::cin >> x;
+        in >> heap.insert(x);
+        return in;
+    }
+
+*/
+   friend std::ostream& operator << (std::ostream& out, const heap<Container>& heap){
+
+            for(auto &v:heap.vec) {
+                 out<< v << " ";
+            }
+            return out;
+
+    }
+
+
+};
+
+
+
 #endif //CS1103_PC2_201902_HEAP_H
--- main.cpp
+++ main.cpp
@@ -1,21 +1,29 @@
 #include <iostream>
-
+#include <vector>
+#include <fstream>
 #include "heap.h"
 using namespace std;
 
 int main()
 {
-    heap<int> h;
-    load_from("data.txt", h);
-    cout << "heap" << endl;
-    cout << h;
+
+    heap<vector<int>> h;
+
+    h.load_from();
     h.insert(10);
     h.insert(20);
     h.insert(40);
     h.insert(50);
     h.insert(10);
+    h.insert(10,20,400,1000);
+    h.get_max();
+
     cout << "heap" << endl;
-    cout << h;
+    cout << h<<endl;
+
+    //cin >> h;
+
+    /*
     cout << "heap" << endl;
     cout << h;
     cin >> h;
@@ -26,5 +34,6 @@ int main()
     cout << h;
     load_from("data.txt", h);
     cout << h;
+     */
     return 0;
 }
